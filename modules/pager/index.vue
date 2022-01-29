4e3f43e4a630765a36704e277d894655a5aa631d
xujin <ferrytsui@163.com>
2022-01-29 23:59:53 +0800

    ok

---
+++ modules/pager/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        this is Pager
+    </div>
+</template>   
+
+<script>
+export default {
+    name: 'SpPager',
+    props:{
+        total:{
+            type: Number,
+            required: true   
+        },
+        current:{
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped> 
+</style>
